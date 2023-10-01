43ea67e15191bfff31c5147cd17e5adf80951c6e
anandusasikumar2000 <anandusk2000@gmail.com>
2023-10-02 00:44:00 +0700

    second commit

--- js/jscript.js
+++ js/jscript.js
@@ -3,16 +3,17 @@ function loadDoc() {
     xhttp.onreadystatechange = function()
      {
         if (this.readyState == 1 && this.status == 200) {
-            document.getElementById("demo").innerHTML =  "Server connection established";
+            document.getElementById("demo").innerHTML=  "<img src='img/giphy.gif'>";
           }
         else if (this.readyState == 2 && this.status == 200) {
-            document.getElementById("demo").innerHTML =  "Request Received";
+            document.getElementById("demo").innerHTML =  "<img src='img/loading1.gif'>";
           }
         else if (this.readyState == 3 && this.status == 200) {
-            document.getElementById("demo").innerHTML = " Processing Request";
+            document.getElementById("demo").innerHTML = "<img src='img/loading.gif'>";
           }
         else if (this.readyState == 4 && this.status == 200) {
         document.getElementById("demo").innerHTML = this.responseText;
+        document.getElementById("demo").style.paddingLeft= '30%';
           }
         else {
             document.getElementById("demo").innerHTML = "Error";
